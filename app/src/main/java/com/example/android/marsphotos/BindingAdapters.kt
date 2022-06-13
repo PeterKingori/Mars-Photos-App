59e90a918d9aff468a0c7457cb0f42e654cbeaa0
Peter Kingori <kpndegwa@gmail.com>
2022-06-13 16:09:54 +0300

    add statuses and a binding adapter to take care of error handling.

--- app/src/main/java/com/example/android/marsphotos/BindingAdapters.kt
+++ app/src/main/java/com/example/android/marsphotos/BindingAdapters.kt
@@ -1,22 +1,27 @@
 package com.example.android.marsphotos
 
+import android.view.View
 import android.widget.ImageView
 import androidx.core.net.toUri
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
 import coil.load
 import com.example.android.marsphotos.network.MarsPhoto
+import com.example.android.marsphotos.overview.MarsApiStatus
 import com.example.android.marsphotos.overview.PhotoGridAdapter
 
 /**
  * Binding Adapters are annotated methods used to create custom setters for custom properties
  * of a view.
  * imageUrl - attribute name in grid_view_item.xml
- * listData - attribute in fragment_overview.xml
+ * listData - attribute in fragment_overview.xml RecyclerView
+ * marsApiStatus - attribute in fragment_overview.xml ImageView
+ */
+
+ /**
  * @param imgView - type of target view
  * @param imgUrl - value being set to the view attribute
  */
-
 @BindingAdapter("imageUrl")
 fun bindImage(imgView: ImageView, imgUrl: String?) {
     imgUrl?.let {
@@ -40,4 +45,26 @@ fun bindImage(imgView: ImageView, imgUrl: String?) {
 fun bindRecyclerView(recyclerView: RecyclerView, marsPhotos: List<MarsPhoto>?) {
     val adapter = recyclerView.adapter as PhotoGridAdapter
     adapter.submitList(marsPhotos)
+}
+
+/**
+ * This is called when the status property in the ViewModel changes.
+ * @param statusImageView - type of target view
+ * @param status - value being set to the view attribute
+*/
+@BindingAdapter("marsApiStatus")
+fun bindStatus(statusImageView: ImageView, status: MarsApiStatus?) {
+    when (status) {
+        MarsApiStatus.LOADING -> {
+            statusImageView.visibility = View.VISIBLE
+            statusImageView.setImageResource(R.drawable.loading_animation)
+        }
+        MarsApiStatus.ERROR -> {
+            statusImageView.visibility = View.VISIBLE
+            statusImageView.setImageResource(R.drawable.ic_connection_error)
+        }
+        MarsApiStatus.DONE -> {
+            statusImageView.visibility = View.GONE
+        }
+    }
 }
